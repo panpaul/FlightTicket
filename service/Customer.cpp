f4af986302545643808cfcf8e34f53dcb0edc067
Paul <1323564116@qq.com>
2020-06-05 21:57:19 +0800

    [service]update Customer

---
+++ service/Customer.cpp
@@ -0,0 +1,147 @@
+#include "Customer.h"
+#include <cstring>
+#include <iostream>
+#include <ctime>
+
+Customer::Customer(char* name, char* id)
+{
+	int lenName = strlen(name);
+	int lenId = strlen(id);
+	if (lenName < (CUSTOMER_NAME_MAX_SIZE / sizeof(char)) && (lenId < (CUSTOMER_ID_MAX_SIZE / sizeof(id))))
+	{
+		strcpy(this->Name, name);
+		strcpy(this->Id, id);
+		QueryInfo();
+	}
+	else
+	{
+		throw std::invalid_argument("姓名或身份信息长度不合法");
+	}
+}
+
+Customer::~Customer()
+= default;
+
+bool Customer::InsertCustomer()
+{
+	db::Customer customer{};
+	strcpy(customer.Name, this->Name);
+	strcpy(customer.Id, this->Id);
+	int id = engine->InsertCustomer(customer);
+	if (id == -1)return false;
+	CustomerId = id;
+}
+
+void Customer::QueryInfo()
+{
+	db::Customer customer{};
+	customer.CustomerId = 0;
+	strcpy(customer.Name, Name);
+	strcpy(customer.Id, Id);
+	auto vec = engine->QueryCustomer(customer);
+	if (vec.empty())
+	{ // new customer
+		InsertCustomer();
+	}
+	else
+	{
+		CustomerId = vec[0].CustomerId;
+	}
+}
+
+void Customer::PrintTickets()
+{
+	db::Order order{ 0, CustomerId, 0, 0 };
+	auto orderVec = engine->QueryOrder(order);
+
+	for (auto i:orderVec)
+	{
+		db::Flight flight{ i.FlightId };
+		auto flightVec = engine->QueryFlight(flight);
+		std::cout << "-------------------------" << std::endl;
+		std::cout << "航班号: " << flightVec[0].FlightName << std::endl;
+		std::cout << "始发地: " << flightVec[0].Departure << std::endl;
+		std::cout << "目的地: " << flightVec[0].Destination << std::endl;
+		char mbstr[100];
+		if (std::strftime(mbstr, sizeof(mbstr), "%a %H:%M:%S", std::localtime(&flightVec[0].Time)))
+		{
+			std::cout << "出发时间: " << mbstr << '\n';
+		}
+		std::cout << "-------------------------" << std::endl;
+	}
+}
+
+void Customer::MakeOrder(char* flightName)
+{
+	if (strlen(flightName) >= (FLIGHT_NAME_MAX_SIZE / sizeof(char)))
+	{
+		//throw std::invalid_argument("航班名称不合法");
+		std::cout << "航班名称不合法" << std::endl;
+		return;
+	}
+
+	db::Flight flight{};
+	strcpy(flight.FlightName, flightName);
+	auto flightVec = engine->QueryFlight(flight);
+	if (flightVec.empty())
+	{
+		//throw std::invalid_argument("航班名称不存在，请检查输入");
+		std::cout << "航班名称不存在，请检查输入" << std::endl;
+		return;
+	}
+
+	if (flightVec[0].MaxCapacity - flightVec[0].Current <= 0)
+	{
+		std::cout << "本航班已满员" << std::endl;
+		return;
+	}
+
+	db::Order order{ 0, CustomerId, flightVec[0].FlightId, flightVec[0].Current + 1 };
+
+	auto judge = engine->InsertOrder(order);
+	if (judge == -1)
+	{
+		std::cout << "抱歉，航空公司规定对于每次航班一人仅能购买一张票，请重试" << std::endl;
+		return;
+	}
+
+	flight.Current++;
+	engine->UpdateFlight(flight);
+
+}
+
+void Customer::DeleteOrder(char* flightName)
+{
+	if (strlen(flightName) >= (FLIGHT_NAME_MAX_SIZE / sizeof(char)))
+	{
+		std::cout << "航班名称不合法" << std::endl;
+		return;
+	}
+
+	db::Flight flight{};
+	strcpy(flight.FlightName, flightName);
+	auto flightVec = engine->QueryFlight(flight);
+	if (flightVec.empty())
+	{
+		std::cout << "航班名称不存在，请检查输入" << std::endl;
+		return;
+	}
+
+	db::Order order{ 0, CustomerId, flightVec[0].FlightId };
+	auto orderVec = engine->QueryOrder(order);
+
+	if (orderVec.empty())
+	{
+		std::cout << "抱歉，您并没有购买本次航班的票，请检查输入的身份信息及航班" << std::endl;
+		return;
+	}
+
+	bool status = engine->DeleteOrder(orderVec[0].OrderId);
+	if (status)std::cout << "订单删除成功" << std::endl;
+	else
+		std::cout << "订单删除失败" << std::endl;
+
+	flightVec[0].Current--;
+	engine->UpdateFlight(flightVec[0]);
+}
+
--- service/service.h
+++ service/service.h
@@ -1,13 +1,13 @@
-//
-// Created by panyuxuan on 2020/6/2.
-//
-
 #ifndef _SERVICE_H_
 #define _SERVICE_H_
 
 #include "../model/linearEngine.h"
-#include <string>
-std::string str = ".";
-static auto engine = new db::linearEngine(str);
+
+static db::linearEngine* engine;
+
+void Setup()
+{
+	engine = new db::linearEngine(".");
+}
 
 #endif //_SERVICE_H_
